58ea2d1b1c08d84e401a99d65caaebd3415310bf
VENKATESHA G S <venki354119@gmail.com>
2024-09-19 22:35:28 +0530

    Validation

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Hdjsh asd2324234jghjsd hjsdg sdhk 12212021 idf32432 32423 d34234jh dfh";
+        string pattern = @"(0[1-9]|1[0-2])([0-2][0-9]|3[01])\d{4}";
+        
+        MatchCollection matches = Regex.Matches(input, pattern);
+        
+        foreach (Match match in matches)
+        {
+            string date = match.Value;
+            // Optional: Validate the date
+            if (DateTime.TryParseExact(date, "MMddyyyy", null, System.Globalization.DateTimeStyles.None, out DateTime validDate))
+            {
+                Console.WriteLine($"Valid date found: {validDate.ToString("MM/dd/yyyy")}");
+            }
+        }
+    }
+}
